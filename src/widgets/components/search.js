810517dd4895f54724c8af48edca0e0b562e1f37
Yamile Tabares Marin <yamile08.lytm@gmail.com>
2018-03-16 14:43:42 -0500

    Referencias a Eventos de HTML y Formularios

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -6,12 +6,19 @@ import './search.css';
 //   )
 // }
 
-const Search = () => ( //tambien se puede crear el componente como un arrow function y no es necesario pasarle el return porque el ya lo hace por si solo
-  <form className="Search">
+const Search = (props) => ( //tambien se puede crear el componente como un arrow function y no es necesario pasarle el return porque el ya lo hace por si solo
+  <form
+    className="Search"
+    onSubmit={props.handleSubmit} //se le pasa el evento onSubmit
+  >
     <input
+      ref={props.setRef} //setRef ayuda a almacenar los valores del input
       type="text"
       placeholder="Busca tu video favorito"
       className="Search-input"
+      name="search"
+      onChange={props.handleChange}
+      value={props.value}
     />
   </form>
 )
